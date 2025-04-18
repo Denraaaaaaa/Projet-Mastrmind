9ac40d679b9af5deb126e9704d51f781898c14ee
Noreddine Belmeguenaï <belynord@gmail.com>
2025-04-18 21:53:31 +0200

    Commentaires et modifications

--- codebreaker2.py
+++ codebreaker2.py
@@ -2,31 +2,43 @@ import common
 import random
 
 def init():
+    # Initialisation des variables globales
     global combinaisons_encore_possible 
-    combinaisons_encore_possible = None
     global traite
     traite = []
+    
+    # Pour détecter la deuxième combinaison (cf. compte rendu pour la justification de ce choix)
+    combinaisons_encore_possible = None 
     return
 
 def codebreaker(evaluation):
     global combinaisons_encore_possible
     global traite 
     
+    # Première combinaison (cas de base n°1)
     if evaluation == None :
         combinaison = ''.join(random.choices(common.COLORS, k=common.LENGTH))
         traite.append(combinaison)
         return combinaison
-      
+    
+    # Deuxième combinaison (cas de base n°2)
     elif combinaisons_encore_possible == None :
-        combinaisons_encore_possible = common.donner_possibles(traite[0], evaluation)
         
+        # Création de l'esemble des possibles car l'évaluation en argument est la première
+        combinaisons_encore_possible = common.donner_possibles(traite[-1], evaluation) 
+        
+        # Ici on transforme l'ensemble des combinaisons possible en une liste car la fonction random.choice ne marche pas avec des ensembles
+        combinaison = random.choice(list(combinaisons_encore_possible))
+        traite.append(combinaison)
+        
+    # Cas général 
     else :
         combinaisons_encore_possible = common.maj_possibles(combinaisons_encore_possible, traite[-1], evaluation)
+        
+        # Idem ici
+        combinaison = random.choice(list(combinaisons_encore_possible))
+        traite.append(combinaison)
     
-    # Ici on transforme l'ensemble des combinaisons possible en une liste car la fonction random.choice ne marche pas avec des ensembles
-    combinaison = random.choice(list(combinaisons_encore_possible))
-    print(combinaisons_encore_possible)
-    traite.append(combinaison)
     return combinaison
 
 
--- codemaker2.py
+++ codemaker2.py
@@ -3,10 +3,13 @@ import random
 import itertools
 
 def init():
+    # Initialisation des variables globales
     global solution
     solution = ''
     global solutions_encore_possible
-    produit_cartesien = list(itertools.product(common.COLORS, repeat = common.LENGTH))  # Produit cartésien de COLORS avec elle même LENGTH-fois dans une liste. Ce qui donc l'ensemble des combinaisons de taille LENGTH possibles de coleurs de COLORS
+    
+    # Produit cartésien de COLORS avec elle même LENGTH-fois dans une liste.
+    produit_cartesien = list(itertools.product(common.COLORS, repeat = common.LENGTH))  
     solutions_encore_possible = set(''.join(tup) for tup in produit_cartesien)
     
 def codemaker(combinaison):
@@ -14,14 +17,19 @@ def codemaker(combinaison):
     global solutions_encore_possible
     assert type(combinaison) == str
     assert type(solution) == str
-        
+
     for solution_possible in solutions_encore_possible:
         maximum = -1 # Maximum négatif pour être sur que n'importe quelle première solution sera acceptée car n >= 0.
+        
+        # Ensemble des solutions qui seront encore possible après avoir proposé la combinaison si solution_possible est solution.
         encore_possible_partiel = common.maj_possibles(solutions_encore_possible, combinaison, common.evaluation(combinaison, solution_possible))
         n = len(encore_possible_partiel)
+        
+        # Recherche de la solution donnant le plus de possiblités 
         if n >= maximum:
             maximum = n
-            solutions_encore_possible = encore_possible_partiel
             solution = solution_possible
             
+    # Mise à jour des solutions encore possibles après l'évaluation
+    solutions_encore_possible = common.maj_possibles(solutions_encore_possible, combinaison, common.evaluation(combinaison, solution))
     return common.evaluation(combinaison, solution)
